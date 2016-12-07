b978dd3f13a263db5d8a9dabf8c0efcbb5202a99
Ryan Teves <rteves18@gmail.com>
2016-12-07 03:10:19 -0800

    close to working. not quite yet

--- santacruz.js
+++ santacruz.js
@@ -19,6 +19,19 @@ var tooltip_json_files = ['sc_tenure_onwer.json','sc_tenure_renter.json'];
 var fieldtest = ['ten','eleven','twelve','thirteen','fourteen'];
 var counter = 0;
 
+//add for legend scale color reference
+var hos = colorbrewer.Oranges[9];
+var tnr = colorbrewer.Greens[9]; 
+var rnt = colorbrewer.Purples[9];
+var val = colorbrewer.Reds[9];
+var ict = colorbrewer.PuOr[9];
+
+var hosText = ["0-40K", "40K-80K", "80K-120K", "120K-160K","160K-200K","200K+"];
+var tnrText = ["0-60", "60-70", "70-80", "80-90", "90-100"];
+var rntText = ["$0-$5000", "$5000-15k", "$15k-$40k", "$40k-$80k", "$80k-$100k"];
+var valText = ["0-10", "10-20", "20-30", "30-40", "40-50", "50-60", "60+"];
+var ictText = ["0-10", "10-20", "20-30", "30-40", "40-50", "50-60", "60+"];
+
 var jsonArrayCounter = 10;
 var running = false;
 var timer;
@@ -67,31 +80,7 @@ var yellow_color = d3.scale.threshold()
 var color = orange_color;
 
 var g;
-
-//d3.select("input.color").on("click", toggleColor);
-d3.select("input.boundary").on("click", toggleBoundary);
-
-//Toggle color range between Red and Blue
-/*function toggleColor() {
-
-  changeColor = !changeColor;
-
-  if(changeColor){  
-    color = blue_color;
-  }
-  else {
-    color = orange_color;
-  }
-  
-  refresh(); //refresh map  
-  
-}*/
-
-//Toggle showBoundary boolean true or false
-function toggleBoundary(){
-  showBoundary = !showBoundary;
-  refresh(); //refresh map
-}
+//an SVG to appen
 
 // A position encoding for the key only.
 var x = d3.scale.linear()
@@ -114,32 +103,39 @@ function refresh() {
   if (firstLoad) {
     legendText = "Housing Unit";
     firstLoad = false;
+    cssLegend = "hoslegend";
+    select_colors=orange_color;  
+    ShowLegendHRVIT(1,0,0,0);
   } else {
     g.selectAll("*")
        .remove();
   }
     
-  g = svg.append("g")
-    .attr("class", "key")
-    .attr("transform", "translate(440,40)");
+  // g = svg.append("g")
+  //   .attr("class", "key")
+  //   .attr("transform", "translate(440,40)");
 
-  g.selectAll("rect")
-    .data(color.range().map(function(d, i) {
-      return {
-        x0: i ? x(color.domain()[i - 1]) : x.range()[0],
-        x1: i < color.domain().length ? x(color.domain()[i]) : x.range()[1],
-        z: d
-      };
-  }))
-  .enter().append("rect")
-    .attr("height", 8)
-    .attr("x", function(d) { return d.x0; })
-    .attr("width", function(d) { return d.x1 - d.x0; })
-    .style("fill", function(d) { return d.z; });
-  g.call(xAxis).append("text")
-    .attr("class", "caption")
-    .attr("y", -6)
-    .text(legendText);
+  // g.selectAll("rect")
+  //   .data(color.range().map(function(d, i) {
+
+  //     return {
+  //       x0: i ? x(color.domain()[i - 1]) : x.range()[0],
+  //       x1: i < color.domain().length ? x(color.domain()[i]) : x.range()[1],
+  //       z: d
+  //     };
+  // }))
+  // .enter().append("rect")
+  //   .attr("fill-opacity", 0)
+  //   .transition().duration(1000)
+  //   .attr("fill-opacity", 1)
+  //   .attr("height", 8)
+  //   .attr("x", function(d) { return d.x0; })
+  //   .attr("width", function(d) { console.log(d.x1-d.x0);  if(Math.abs(d.x1-d.x0) > 480) return d.hope; else return Math.abs(d.x1-d.x0); })
+  //   .style("fill", function(d) { return d.z; });
+  //   g.call(xAxis).append("text")
+  //   .attr("class", "caption")
+  //   .attr("y", -6)
+  //   .text(legendText);
 
   // Loads the json files to render map
   d3.json(json_files[current_json_file], function(error, sc){
@@ -153,7 +149,8 @@ function refresh() {
       .append("path")
         .datum(topojson.feature(sc, sc.objects.sctracts))
         .attr("d", path);
-
+        if(firstLoad)
+          first_Load();
     // Individual tracts for tool tip.
     
     // Group tracts by color for faster rendering.
@@ -209,13 +206,13 @@ function refresh() {
 
     // Draw county borders.
     //Toggle show boundary
-    if(showBoundary){
       svg.append("path")
         .datum(topojson.mesh(sc, sc.objects.sctracts, function(a, b) { return a !== b; }))
         .attr("class", "county-border")
         .attr("d", path);
-    }
+    
   });
+
 }
 
 // ** Update data section (Called from the onclick)
@@ -239,6 +236,108 @@ function updateData() {
     refresh();
 
 }
+
+function AppendLegend(cScale, brewSet, textArray,cssClass,opacity){
+    var legendHeight=600;
+      //   svg.append("g")
+      //   .attr("class", "key")
+      //   .data(color.range().map(function(d, i) { return {
+      //   x0: i ? x(color.domain()[i - 1]) : x.range()[0],
+      //   x1: i < color.domain().length ? x(color.domain()[i]) : x.range()[1],
+      //   z: d
+      // };
+      // }))
+      //   .style("fill", function(d) { return d.z; });
+        svg.selectAll(".legend")
+        .data(cScale.domain(),function(d){return d;})
+        .enter()
+        .append("g")
+        .attr("transform", "translate(-550,40)")
+        .attr("class", "key")
+        .attr("opacity",opacity)
+        .append("rect")//55*i
+        //sets the location and width of each colored rectangles and adds the iteratively
+        .attr("x", 600)
+        .attr("y", function(d,i){return height-175-(55*i)})
+        .attr("width", 15)
+        .attr("height", 55)
+        .attr("fill", function(d, i){ return brewSet[i];})
+        .style("stroke", "rgba(105, 105, 105, 0.86)")
+        .style("stroke-width", "2px")
+        .style("opacity",1)
+    //further appending will append it inside rect. Starting again appending to g.
+    svg.selectAll("g."+cssClass)
+        .append("text")
+        .attr("class", cssClass)
+        .attr("x", function(d){ return 600+25})
+        .attr("y", function(d,i){return height-140-(55*i)})
+        .attr("width", 200)
+        .attr("height", 15)
+        .style("opacity",opacity)
+        .style("fill", "black")
+        .style("font-weight", "bold")
+        .style("font-family", "sans-serif")
+        .style("font-size", 10)
+        .text(function(d, i) { return (textArray[i]);});
+    //Legend Text
+    svg.select("g."+cssClass)
+        .append("text")
+        .attr("class", cssClass)
+        .attr("id","legendText")
+        .attr("x", 585)
+        .attr("y", height-90)
+        .attr("width", 200)
+        .attr("height", 15)
+        .attr("align","center")
+        .style("opacity",opacity)
+        .style("fill", "black")
+        .style("font-weight", "bold")
+        .style("font-family", "sans-serif")
+        .style("font-size", 12)
+        .text(function(){
+            switch(cssClass){
+                case "hoslegend":
+                    return "Housing Unit Total per Mile";
+                    break;
+                case "tnrlegend":
+                    return "Tenure Total per Mile";
+                    break;
+                case "rntlegend":
+                    return "Median Contract Rent (USD)";
+                case "vallegend":
+                    return "Median House Value (USD)";
+                    break;
+                case "ictlegend":
+                    return "Median Income (USD)";
+                    break;
+            }
+        })
+}
+
+function ShowLegendHRVIT(hu,rt,vl,ic,tn){
+    d3.selectAll(".hoslegend")
+        .transition(1000)
+        .style("opacity",hu);
+    d3.selectAll(".rntlegend")
+        .transition(1000)
+        .style("opacity",rt);
+    d3.selectAll(".vallegend")
+        .transition(1000)
+        .style("opacity",vl);
+    d3.selectAll(".ictlegend")
+        .transition(1000)
+        .style("opacity",ic);
+    d3.selectAll(".tnrlegend")
+        .transition(1000)
+        .style("opacity",tn);
+}
+
+//Gets the legend started. Hidden
+AppendLegend(orange_color,hos,hosText,"hoslegend",0);
+AppendLegend(green_color,tnr,tnrText,"tnrlegend",0);
+AppendLegend(purple_color,rnt,rntText,"rntlegend",0);
+AppendLegend(red_color,val,valText,"vallegend",0);
+AppendLegend(yellow_color,ict,ictText,"ictlegend",0);
 
 function getToolTipLabel(d) {
 
@@ -266,7 +365,7 @@ function getToolTipLabel(d) {
 
   var finalLabel = "<strong><div style='text-align:center;'>" +  
                  legendText
-              + "</div></strong>"
+              + "</div></strong></br>"
               //
               + "<table><tr><th>Year</th><th>Total</th></tr>"
               + "<tr>" + "<tr><th>"
@@ -274,26 +373,6 @@ function getToolTipLabel(d) {
               +"</th><th><font color ='" + label_color + "'>" + sign 
               + year_data;
 
-  var values = "<br/>" + d.values[0].properties.ten 
-              + "<br/>" + d.values[0].properties.eleven  
-              + "<br/>" + d.values[0].properties.twelve 
-              + "<br/>" + d.values[0].properties.thirteen 
-              + "<br/>" + d.values[0].properties.fourteen;
- /*if (this.current_value_dropdown == "housing_unit") {
-    return "housing_unit<br/>" +"key: "+ d.key + "<br/>values: " + values;
- } else if (this.current_value_dropdown == "median_contract_rent") {
-    return "median_contract_rent<br/>" +"key: "+ d.key + "<br/>values: "+values;    
- } else if (this.current_value_dropdown == "median_value") {
-    return "median_value<br/>" +"key: "+ d.key +"<br/>values: "+ values;      
- } else if (this.current_value_dropdown == "median_year_miubt") {
-    return "median_year_miubt<br/>" +"key: "+ d.key +"<br/>values: "+ values;     
- } else if (this.current_value_dropdown == "median_income") {
-    return "median_income<br/>" +"key: "+ d.key +"<br/>values: " + values;    
- } else if (this.current_value_dropdown == "tenure") {
-    return "tenure<br/>" +"key: "+ d.key +"<br/>values: "+ values;      
- } else {
-     alert("Error: option data unavailable: " + this.current_value_dropdown);
- } */   
   return finalLabel;
 }
 
@@ -309,6 +388,7 @@ document.getElementById("myList").onchange = function() {
     legendText = "Housing Unit";
     sign = "";
     label_color= "#2756a3";
+    ShowLegendHRVIT(1,0,0,0,0);
     refresh();
   } else if (this.value == "median_contract_rent") {
     current_json_file = 2;
@@ -316,6 +396,7 @@ document.getElementById("myList").onchange = function() {
     legendText = "Median Rent (USD)";
     sign = "$";
     label_color = "#006837";
+    ShowLegendHRVIT(0,1,0,0,0);
     refresh();       
   } else if (this.value == "median_value") {
     current_json_file = 3;
@@ -323,6 +404,7 @@ document.getElementById("myList").onchange = function() {
     legendText = "Median Value (USD)";
     sign = "$";
     label_color = "#006837";
+    ShowLegendHRVIT(0,0,1,0,0);
     refresh();            
   } else if (this.value == "median_income") {
     current_json_file = 4;
@@ -330,6 +412,7 @@ document.getElementById("myList").onchange = function() {
     legendText = "Median Income (USD)";
     sign = "$";
     label_color = "#006837";
+    ShowLegendHRVIT(0,0,0,1,0);
     refresh();       
   } else if (this.value == "tenure") {
     current_json_file = 1;
@@ -337,6 +420,7 @@ document.getElementById("myList").onchange = function() {
     legendText = "Tenure";
     sign = "";
     label_color = "#2756a3";
+    ShowLegendHRVIT(0,0,0,0,1);
     refresh();       
   } else {
     alert("Error: option data unavailable");
@@ -346,7 +430,7 @@ document.getElementById("myList").onchange = function() {
 
 $("button").on("click", function() {
 		
-    var duration = 3000,
+    var duration = 1000,
         maxstep = 2014,
         minstep = 2010;
     
@@ -394,6 +478,10 @@ $("#slider").on("change", function(){
     clearInterval(timer);
     $("button").html("Play");
 });
+
+function first_Load(){
+    
+}
 	
 update = function() {
 
